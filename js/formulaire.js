9bcd39e00fa1d4c7a8a54f8d56692d79dd286174
Vladimir Rekai <vladimir.rekai@etu.unicaen.fr>
2023-09-24 21:23:37 +0200

    :feat: Activation du champ commune si cp rempli

--- js/formulaire.js
+++ js/formulaire.js
@@ -5,3 +5,15 @@
 //Éléments du document html
 const cp_entre = document.getElementById("code_postal")
 const commune_entre = document.getElementById("commune")
+
+cp_entre.addEventListener('change',()=>{
+    console.log("listener !")
+    let entre = "" +cp_entre.value
+    let nb_cp = parseInt(entre)
+    console.log(nb_cp)
+    if(nb_cp > 9999){
+        commune_entre.disabled = false
+    }else{
+        commune_entre.disabled = true
+    }
+});
