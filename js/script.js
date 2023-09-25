f164d85bde1df43a96a47832df6e60551d37e598
emeline <emeline.laigle@unicaen.fr>
2023-09-25 08:39:59 +0200

    fix: correction bug date et nom ville #5

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
 
-//const communeSelect = document.getElementById('communeSelect');
-//let rechercher=document.getElementById('rechercher');
+const communeSelect = document.getElementById('communeSelect');
 
-//rechercher.addEventListener('click',()=>{
-//    traitementMeteo();
-//});
 
-window.onload= ()=>{
-    traitementMeteo();
-}
+
 
 function traitementMeteo(){
-    //const commune= communeSelect.value;
+    const commune= communeSelect.value;
     //if(commune!=null){
-        let city='Rouen';
-        let url ='https://api.meteo-concept.com/api/forecast/daily/0?token=78566df65679384cb5aef99952feed7743b1c622f6cf67fb152c48716c77bce9&insee='+city;
-        fetch(url).then(response=> 
+        fetch(`https://api.meteo-concept.com/api/forecast/daily/0?token=a40f714d88661e3a21d0b715f5867d5953a2231d2b9beefa5838800b5fc40d8d&insee=${commune}`).then(response=> 
             response.json().then(data => {
                 console.log(data);
                 document.getElementById('tMin').textContent=data.forecast.tmin;
@@ -25,11 +17,15 @@ function traitementMeteo(){
                 document.getElementById('longitude').textContent=data.city.longitude;
                 let date= new Date(Date.now());
                 document.getElementById('date').textContent=date;
+                document.getElementById('ville').textContent=data.city.name;
                 //document.getElementById('ensoleillement').textContent='${displayHours(data.forecast.sun_hours)}';
             })
         ).catch((err)=>console.log('Erreur : '+ err));
     //}
 }
+let envoyer = document.getElementById('envoyer').addEventListener('click',()=>{
+    traitementMeteo();
+})
 
 
 
