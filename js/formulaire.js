3fa1a71e3b090d6a319e3e21b6bf1abeabd0157e
Vladimir_rekai <vladimir.rekai.etu.unicaen.fr>
2023-09-25 08:27:53 +0200

    :docs: commentaire de l'écouteur

--- js/formulaire.js
+++ js/formulaire.js
@@ -6,14 +6,16 @@
 const cp_entre = document.getElementById("code_postal")
 const commune_entre = document.getElementById("commune")
 
+/**
+ * Écouteur sur le champ du code postal, permet d'activer le champ Commune lorsqu'un code postal valide est rentré. 
+ */
 cp_entre.addEventListener('change',()=>{
-    console.log("listener !")
-    let entre = "" +cp_entre.value
-    let nb_cp = parseInt(entre)
-    console.log(nb_cp)
+    let nb_cp = parseInt(cp_entre.value)
+
     if(nb_cp > 9999){
         commune_entre.disabled = false
     }else{
         commune_entre.disabled = true
     }
+    
 });
