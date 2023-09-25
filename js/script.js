3bd6f00e2015dbf4909f5e046b288eea613ddbe5
emeline <emeline.laigle@unicaen.fr>
2023-09-25 09:29:37 +0200

    docs: ajout documentation js api meteo

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
-
+// Sélectionne l'élément HTML avec l'ID 'communeSelect'
 const communeSelect = document.getElementById('communeSelect');
 
 
-
-
+// Définit une fonction de traitement pour la météo
 function traitementMeteo(){
+    // Récupère la valeur sélectionnée dans la liste déroulante communeSelect
     const commune= communeSelect.value;
-    //if(commune!=null){
+    if(commune!=null){
+            // Effectue une requête fetch pour obtenir les données météorologiques
         fetch(`https://api.meteo-concept.com/api/forecast/daily/0?token=a40f714d88661e3a21d0b715f5867d5953a2231d2b9beefa5838800b5fc40d8d&insee=${commune}`).then(response=> 
             response.json().then(data => {
                 console.log(data);
-                document.getElementById('tMin').textContent=data.forecast.tmin;
-                document.getElementById('tMax').textContent=data.forecast.tmax;
-                document.getElementById('direction').textContent=data.forecast.probarain;
-                document.getElementById('latitude').textContent=data.city.latitude;
-                document.getElementById('longitude').textContent=data.city.longitude;
+                document.getElementById('tMin').textContent=data.forecast.tmin; //met à jour la température minimal de la ville sélectionnée 
+                document.getElementById('tMax').textContent=data.forecast.tmax; //met à jour la température maximal de la ville sélectionnée 
+                document.getElementById('probabilite').textContent=data.forecast.probarain+ "%"; //met à jour la probabilité de pluie en % de la ville sélectionnée 
+                document.getElementById('latitude').textContent=data.city.latitude; //met à jour la latitude de la ville sélectionnée 
+                document.getElementById('longitude').textContent=data.city.longitude; //met à jour la longitude de la ville sélectionnée 
                 let date= new Date(Date.now());
-                document.getElementById('date').textContent=date;
-                document.getElementById('ville').textContent=data.city.name;
-                //document.getElementById('ensoleillement').textContent='${displayHours(data.forecast.sun_hours)}';
+                document.getElementById('date').textContent=date; //met à jour la date d'aujourd'hui de la ville sélectionnée 
+                document.getElementById('ville').textContent=data.city.name; //met à jour le nom de la ville sélectionnée sur la page html 
+                document.getElementById('vitesse').textContent=data.forecast.wind10m+" km/h"; //met à jour la vitesse du vent de la ville sélectionnée 
+                document.getElementById('tempsSoleil').textContent=data.forecast.sun_hours +" h"; //met à jour le temps d'ensoleillement de la ville sélectionnée 
+                document.getElementById('direction').textContent=data.forecast.dirwind10m+" °"; //met à jour le degrès de direction du vent de la ville sélectionnée 
+                document.getElementById('precipitation').textContent=data.forecast.rr10+" mm";//met à jour le cumul de pluie sur la journée en mm de la ville sélectionnée 
             })
         ).catch((err)=>console.log('Erreur : '+ err));
-    //}
+    }
 }
+
+// Sélectionne l'élément HTML avec l'ID 'envoyer' et ajoute un écouteur d'événements 'click'
 let envoyer = document.getElementById('envoyer').addEventListener('click',()=>{
+        // Appelle la fonction de traitementMeteo lorsque le bouton est cliqué
     traitementMeteo();
 })
 
