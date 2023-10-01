7ddab5b48b0511868afbf469d2ab495b4a009af8
emeline <emeline.laigle@unicaen.fr>
2023-10-01 19:05:42 +0200

    feat: fonctionnalité de la meteo sur plusieurs j

--- js/script.js
+++ js/script.js
@@ -7,13 +7,19 @@ const SelectVille = document.getElementById("SelectVille");
 const nbjour = document.getElementById("nbjour");
 const depardieu = document.getElementById("depardieu");
 const SelectJour = document.getElementById("SelectJour");
+const envoieNbJours = document.getElementById("envoyer2");
 
 
 
 // Sélectionne l'élément HTML avec l'ID 'envoyer' et ajoute un écouteur d'événements 'click'
 document.getElementById('envoyer').addEventListener('click',()=>{
     // Appelle la fonction de traitementMeteo lorsque le bouton est cliqué
-traitementMeteo();
+    traitementMeteo();
+})
+
+envoieNbJours.addEventListener('click',()=>{
+    //Appelle de la fonction de traitement meteo en fonction du jours choisi
+    traitementMeteoJours();
 })
 
 
@@ -90,11 +96,11 @@ function traitementMeteo(){
     const commune= communeSelect.value;
     if(commune!=null){
             // Effectue une requête fetch pour obtenir les données météorologiques
-        fetch(`https://api.meteo-concept.com/api/forecast/daily/0?token=a40f714d88661e3a21d0b715f5867d5953a2231d2b9beefa5838800b5fc40d8d&insee=${commune}`).then(response=> 
+        fetch(`https://api.meteo-concept.com/api/forecast/daily/0?token=78566df65679384cb5aef99952feed7743b1c622f6cf67fb152c48716c77bce9&insee=${commune}`).then(response=> 
             response.json().then(data => {
                 console.log(data);
-                document.getElementById('tMin').textContent=data.forecast.tmin; //met à jour la température minimal de la ville sélectionnée 
-                document.getElementById('tMax').textContent=data.forecast.tmax; //met à jour la température maximal de la ville sélectionnée 
+                document.getElementById('tMin').textContent=data.forecast.tmin + "°C"; //met à jour la température minimal de la ville sélectionnée 
+                document.getElementById('tMax').textContent=data.forecast.tmax + "°C"; //met à jour la température maximal de la ville sélectionnée 
                 document.getElementById('probabilite').textContent=data.forecast.probarain+ "%"; //met à jour la probabilité de pluie en % de la ville sélectionnée 
                 document.getElementById('latitude').textContent=data.city.latitude; //met à jour la latitude de la ville sélectionnée 
                 document.getElementById('longitude').textContent=data.city.longitude; //met à jour la longitude de la ville sélectionnée 
@@ -105,6 +111,33 @@ function traitementMeteo(){
                 document.getElementById('tempsSoleil').textContent=data.forecast.sun_hours +" h"; //met à jour le temps d'ensoleillement de la ville sélectionnée 
                 //document.getElementById('direction').textContent=data.forecast.dirwind10m+" °"; //met à jour le degrès de direction du vent de la ville sélectionnée 
                 //document.getElementById('precipitation').textContent=data.forecast.rr10+" mm";//met à jour le cumul de pluie sur la journée en mm de la ville sélectionnée 
+            })
+        ).catch((err)=>console.log('Erreur : '+ err));
+    }
+}
+
+// Définit une fonction de traitement pour la météo
+function traitementMeteoJours(){
+    // Récupère la valeur sélectionnée dans la liste déroulante communeSelect
+    const commune= communeSelect.value;
+    const jour=SelectJour.value;
+    if(commune!=null){
+            // Effectue une requête fetch pour obtenir les données météorologiques pour le jour choisi et la ville choisi
+        fetch(`https://api.meteo-concept.com/api/forecast/daily?token=78566df65679384cb5aef99952feed7743b1c622f6cf67fb152c48716c77bce9&insee=${commune}`).then(response=> 
+            response.json().then(data => {
+                console.log(data);
+                document.getElementById('tMin').textContent=data.forecast[jour].tmin + "°C"; //met à jour la température minimal de la ville sélectionnée 
+                document.getElementById('tMax').textContent=data.forecast[jour].tmax + "°C"; //met à jour la température maximal de la ville sélectionnée 
+                document.getElementById('probabilite').textContent=data.forecast[jour].probarain + "%"; //met à jour la probabilité de pluie en % de la ville sélectionnée 
+                document.getElementById('latitude').textContent=data.city.latitude; //met à jour la latitude de la ville sélectionnée 
+                document.getElementById('longitude').textContent=data.city.longitude; //met à jour la longitude de la ville sélectionnée 
+                let date= new Date(data.forecast[jour].datetime);
+                document.getElementById('date').textContent=creer_date(date); //met à jour la date d'aujourd'hui de la ville sélectionnée 
+                document.getElementById('ville').textContent=data.city.name; //met à jour le nom de la ville sélectionnée sur la page html 
+                //document.getElementById('vitesse').textContent=data.forecast.wind10m+" km/h"; //met à jour la vitesse du vent de la ville sélectionnée 
+                document.getElementById('tempsSoleil').textContent=data.forecast[jour].sun_hours +" h"; //met à jour le temps d'ensoleillement de la ville sélectionnée 
+                //document.getElementById('direction').textContent=data.forecast.dirwind10m+" °"; //met à jour le degrès de direction du vent de la ville sélectionnée 
+                //document.getElementById('precipitation').textContent=data.forecast.rr10+" mm";//met à jour le cumul de pluie sur la journée en mm de la ville sélectionnée */
             })
         ).catch((err)=>console.log('Erreur : '+ err));
     }
